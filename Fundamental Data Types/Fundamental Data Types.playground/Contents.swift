c89aaa507584b227350326bdcf12da07b48498e4
Dmitrii Pekhterev <dmpehterev@gmail.com>
2024-09-02 16:15:02 +0300

    learning fundamental data types

--- Fundamental Data Types/Fundamental Data Types.playground/Contents.swift
+++ Fundamental Data Types/Fundamental Data Types.playground/Contents.swift
@@ -3,3 +3,40 @@ import UIKit
 let someText = "Я учу свфит"
 
 let someText1: String = "Я учу свифт"
+
+
+var orcsName = "Рурхард"
+
+var elfsName: String = "Эанор"
+
+orcsName = "Гомри"
+elfsName = "Лиасель"
+
+print(elfsName)
+print(orcsName)
+
+
+
+
+// неявно определим параметр целочисленного типа данных
+var variableOne = 23
+// явно определим параметр целочисленного типа данных
+// после чего передадим ему значение другого параметра в качестве
+// первоначального
+let variableOneCopy: Int = variableOne
+print(variableOneCopy)
+// изменим значение в первой переменной
+variableOne = 25
+print(variableOneCopy)
+print(variableOne)
+
+
+var dodik = 28
+
+let dodikCopy: Int = dodik
+print(dodikCopy)
+
+dodik = 30
+//dodikCopy не поменялся потому что выше (var dodik=28 передало это значение в константу "Которая не меняется" те дальше с переменной можно делать все но константа не поменяется)
+print(dodikCopy)
+print(dodik)
